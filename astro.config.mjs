c37dbb8438f6b940a8c1265ff6ee1eabf865e5a1
Hendra Gunawan <48410066+JadlionHD@users.noreply.github.com>
2025-02-23 02:56:33 +0700

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,14 +3,12 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 
-import vue from "@astrojs/vue";
-
 import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://jad.li",
-  integrations: [mdx(), sitemap(), vue()],
+  integrations: [mdx(), sitemap()],
 
   vite: {
     plugins: [tailwindcss()]
