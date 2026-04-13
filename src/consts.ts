448427083eb343f682da284ac4f1b44a4b904938
Hendra Gunawan <jadlion.dev@gmail.com>
2026-04-14 01:43:01 +0700

    feat: change role

--- src/consts.ts
+++ src/consts.ts
@@ -5,7 +5,8 @@ export const SITE_TITLE = "JadlionHD";
 export const SITE_DESCRIPTION = "Welcome to my website!";
 export const SITE_AUTHOR = "Hendra Gunawan";
 export const SITE_AUTHOR_NICKNAME = SITE_TITLE;
-export const SITE_AUTHOR_PROFILE_PICTURE = "https://avatars.githubusercontent.com/u/48410066?v=4";
-export const SITE_AUTHOR_ROLE = ["Fullstacks Developer"];
+export const SITE_AUTHOR_PROFILE_PICTURE =
+  "https://avatars.githubusercontent.com/u/48410066?v=4";
+export const SITE_AUTHOR_ROLE = ["Aya's lover"];
 
 export const AYA_URL = `/aya/117621709_p0.png`;
